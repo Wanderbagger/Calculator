dd89353d546983bf443b22cf7d7ad1a3424dba2e
Wanderbagger <ronikut1988@mail.ru>
2025-03-05 13:54:38 +0300

    Start project

---
+++ src/main/java/ru/shlyakhov/practice/controllers/CalculatorController.java
@@ -0,0 +1,41 @@
+package ru.shlyakhov.practice.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.shlyakhov.springcourse.dao.PersonDAO;
+import ru.shlyakhov.springcourse.models.Person;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/people")
+public class CalculatorController {
+
+
+    @Autowired
+    public CalculatorController(){
+
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+
+}

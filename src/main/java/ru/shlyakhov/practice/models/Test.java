5c61c4ccedad3b8ed3500e29375233c117fbd5aa
Wanderbagger <ronikut1988@mail.ru>
2025-04-02 08:49:06 +0300

    Continue project 02/04/2025

---
+++ src/main/java/ru/shlyakhov/practice/models/Test.java
@@ -0,0 +1,8 @@
+package ru.shlyakhov.practice.models;
+
+public class Test {
+    public static void main(String[] args) {
+        Calculation calculation = new Calculation(0, "5*5*5");
+
+    }
+}
